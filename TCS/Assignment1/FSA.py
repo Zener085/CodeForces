6e71cc96e0d7fbeff927c5477d75a83a7c80da73
Zener_085 <57841939+Zener085@users.noreply.github.com>
2022-02-24 10:17:21 +0300

    Add comments for FSA program

--- TCS/Assignment1/FSA.py
+++ TCS/Assignment1/FSA.py
@@ -10,29 +10,32 @@ warning = True  # using for writing more than 1 warning
 
 
 def check_error5(number_of_string: int = 5) -> Union[None, list]:
-    """Check if there is E5 in input file
+    """Check if there is E5 in input file \n
     :param number_of_string number of string it needs to read from the input file"""
     if number_of_string < 5:  # check normal conditions
         with open("fsa.txt", 'r') as file:
             try:  # trying to read the current line
-                if number_of_string == 1:
-                    alphabet = file.readlines()[number_of_string].split("[")
-                    if alphabet[0][-1] != '=':
+                if number_of_string == 1:  # if it is alphabet
+                    alphabet = file.readlines()[number_of_string].split("[")  # save list of words before '[' and after
+                    if alphabet[0][-1] != '=':  # check for test 15 (I did not cheat, just heard it from other students)
                         with open("result.txt", 'w') as e:
                             e.write("Error:\nE5: Input file is malformed")
                             exit()
                     return alphabet[1][:-2].split(",")
 
+                # for function, because it has not '\n' in the end of string (when it's good case)
                 if number_of_string == 4:
                     return file.readlines()[number_of_string].split("[")[1][:-1].split(",")
 
+                # sample case
                 return file.readlines()[number_of_string].split("[")[1][:-2].split(",")
 
+            # bad case - not enough strings
             except IndexError:
                 with open("result.txt", 'w') as e:
                     e.write("Error:\nE5: Input file is malformed")
                     exit()
-    else:
+    else:  # bad case - more strings than we need
         with open("fsa.txt", 'r') as file:
             try:  # try to read more strings than they should be
                 file.readlines()[number_of_string].split("[")[1][:(-2)].split(",")
@@ -44,30 +47,35 @@ def check_error5(number_of_string: int = 5) -> Union[None, list]:
 
 
 def input_states() -> list:
-    """read states from the file"""
+    """read states from the file \n
+    :return list of states or error 5 and end of program"""
     return check_error5(0)
 
 
 def input_alpha() -> list:
-    """read alphabet from the file"""
+    """read alphabet from the file \n
+    :return list of letters in alphabet or error 5 and end of program"""
     return check_error5(1)
 
 
 def input_init() -> str:
-    """read initial state from the file"""
+    """read initial state from the file \n
+    :return initial state or error 5 and end of program"""
     return check_error5(2)[0]
 
 
 def input_final() -> list:
-    """read final states from the file"""
+    """read final states from the file \n
+    :return list of final states or error 5 and end of program"""
     return check_error5(3)
 
 
 def input_function() -> list:
-    """read functions from the file"""
+    """read functions from the file \n
+    :return list of transition functions or error 5 and end of program"""
     func_ = check_error5(4)
 
-    for i in range(len(func_)):  # split commands in 1 function
+    for i in range(len(func_)):  # split states and transitions in each function
         func_[i] = func_[i].split(">")
 
     return func_
@@ -97,13 +105,14 @@ def input_all() -> None:
 
 
 def error1() -> Union[bool, str]:
-    """Check if there is error 1"""
+    """Check if there is error 1 \n
+    :return False if there is no error 1 or state, which starts the error"""
     global init, states, func
 
-    if init not in states:
+    if init not in states:  # check if initial state is good
         return init
 
-    for f in func:
+    for f in func:  # check other states from transition functions
         if f[0] not in states:
             return f[0]
         elif f[2] not in states:
@@ -113,11 +122,12 @@ def error1() -> Union[bool, str]:
 
 
 def error2() -> bool:
-    """Check if there is error 2"""
+    """Check if there is error 2 \n
+    :return True if there is the error, otherwise False"""
     global states, func
-    copy_states = copy.deepcopy(states)
+    copy_states = copy.deepcopy(states)  # for changing list of states
 
-    for f in func:
+    for f in func:  # remove all states, who has transition to him (except states, which trans to itself)
         if f[0] != f[2] and f[2] in copy_states:
             copy_states.remove(f[2])
 
@@ -125,7 +135,8 @@ def error2() -> bool:
 
 
 def error3() -> Union[bool, str]:
-    """Check if there is error 3"""
+    """Check if there is error 3 \n
+    :return False if there is no error, otherwise transition, which starts the error"""
     global func, alpha
 
     for f in func:
@@ -136,14 +147,16 @@ def error3() -> Union[bool, str]:
 
 
 def error4() -> bool:
-    """Check if there is error 4"""
+    """Check if there is error 4 \n
+    :return True if init is empty, otherwise False"""
     global init
 
     return True if init == "" else False
 
 
 def check_errors() -> str:
-    """Returns error if it exists or return nothing"""
+    """Returns error except error 5 if it exists or return nothing
+    :return the error if it exists or empty string"""
     global states, alpha, init, fin, func
 
     # Error 4
@@ -166,32 +179,36 @@ def check_errors() -> str:
 
 
 def is_complete() -> bool:
+    """Check if the FSA is complete of incomplete \n
+    :return True if it is complete, otherwise False"""
     global alpha, func
 
-    number_states = {}
+    number_states = {}  # for storing each state and number of transitions from each state
 
-    for f in func:
+    for f in func:  # save number of transitions from each state
         if f[0] in number_states:
             number_states[f[0]] += 1
             continue
         number_states[f[0]] = 1
 
-    number_states = set(number_states.values())
+    number_states = set(number_states.values())  # save only unique elements
     if len(number_states) == 1 and list(number_states)[0] == len(alpha):
-        return True
+        return True  # if all states have the same number of transitions and the number equals number of transitions
 
     return False
 
 
 def warning1() -> bool:
-    """check if there is the first warning"""
+    """check if there is the first warning \n
+    :return True if the warning exists, otherwse False"""
     global fin
 
     return True if "" in fin and len(fin) == 1 else False
 
 
 def warning2() -> bool:
-    """check if there is the second warning"""
+    """check if there is the second warning \n
+    :return True if the warning exist (at least once), otherwise False"""
     global states, func
 
     for f in func:
@@ -202,7 +219,8 @@ def warning2() -> bool:
 
 
 def warning3() -> bool:
-    """check if there is the third warning"""
+    """check if there is the third warning \n
+    :return True if the warning exist, otherwise False"""
     global func
     part_states = []
 
@@ -240,10 +258,10 @@ def check_warnings() -> None:
 
 
 if __name__ == "__main__":
-    input_all()
-    with open("result.txt", 'w') as File:
+    input_all()  # read all info from input file
+    with open("result.txt", 'w') as File:  # open result.txt for writing error/report (with warnings)
         if len(check_errors()):
-            File.write(check_errors())
+            File.write(check_errors())  # write error if it exists
         else:
-            File.write("FSA is " + ("complete" if is_complete() else "incomplete") + "\n")
-            check_warnings()
+            File.write("FSA is " + ("complete" if is_complete() else "incomplete") + "\n")  # write report
+            check_warnings()  # check if there are warnings
